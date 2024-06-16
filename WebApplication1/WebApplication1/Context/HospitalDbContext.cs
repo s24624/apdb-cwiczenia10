caf856929fa8cd0f76b4a9bfa62c12b996725951
Maks G <s24624@pjwstk.edu.pl>
2024-06-16 14:35:40 +0200

    Dodano Model i migracje doctor

---
+++ WebApplication1/WebApplication1/Context/HospitalDbContext.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+
+public class HospitalDbContext : DbContext
+    {
+        public HospitalDbContext() { }
+        
+        public virtual DbSet<Doctor> Doctors { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer("Data Source=db-mssql;Initial Catalog=2019SBD;" +
+                                        "Integrated Security=True;TrustServerCertificate=True");
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+            
+            modelBuilder.Entity<Doctor>(entity =>
+            {
+                entity.HasKey(e => e.IdDoctor);
+
+                entity.Property(e => e.FirstName).HasMaxLength(100).IsRequired();
+                entity.Property(e => e.LastName).HasMaxLength(100).IsRequired();
+                entity.Property(e => e.Email).HasMaxLength(100).IsRequired();
+                
+              
+            });
+        }
+    }
